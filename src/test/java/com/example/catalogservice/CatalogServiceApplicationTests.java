f70fd9f7a7443594c967eb7ed530dd300053fa7c
Arkady <git@arkady.de>
2024-04-23 20:57:48 +0200

    fix: application test

--- src/test/java/com/example/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/example/catalogservice/CatalogServiceApplicationTests.java
@@ -4,11 +4,13 @@ import com.example.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("integration")
 class CatalogServiceApplicationTests {
 
     @Autowired
@@ -78,7 +80,9 @@ class CatalogServiceApplicationTests {
                 .exchange()
                 .expectBody(Book.class).value(actualBook -> {
                     assertThat(actualBook).isNotNull();
-                    assertThat(actualBook).isEqualTo(expectedBook);
+                    assertThat(actualBook.title()).isEqualTo(expectedBook.title());
+                    assertThat(actualBook.author()).isEqualTo(expectedBook.author());
+                    assertThat(actualBook.price()).isEqualTo(expectedBook.price());
                 });
     }
 }
